fe21fb7993df938a2521886058c044f1a6db8435
SamuelWade <samuelrcwade@gmail.com>
2023-02-26 13:09:46 -0500

    Test Successful

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names) {
+    let copyNames = [];
+    for (let i=0; i<names.length;i++) {
+        copyNames.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return copyNames;
+
+}
+
+function countDown(n) {
+    let total = 0;
+    for (let i=n; i!=-1; i--) {
+        console.log(i);
+        total+=i;
+    }
+}
